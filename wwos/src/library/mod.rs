eeb10aa416be21682fe0f7214c535bcba7191cf6
Weihao Wang <weihaow@outlook.com>
2024-11-02 18:08:41 +0800

    remove rust version

--- wwos/src/library/mod.rs
+++
@@ -1,9 +0,0 @@
-mod device_init;
-mod device_tree;
-mod memory_aligned_array;
-mod memory_mapping;
-
-pub use device_init::*;
-pub use device_tree::*;
-pub use memory_aligned_array::*;
-pub use memory_mapping::*;
